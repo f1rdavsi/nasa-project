896c3cabfc9e3966f6e19317c09c383773090b00
Firdavsi <fozilovfirdavsi05@gmail.com>
2025-10-05 21:35:36 +0500

    fix

--- frontend/space-labs/src/app/historical/page.tsx
+++ frontend/space-labs/src/app/historical/page.tsx
@@ -1,18 +1,19 @@
+'use client'
 import Historical from '@/entities/components/Historical'
 import Footer from '@/widgets/footer/footer'
 import Header from '@/widgets/header/header'
 import { ParallaxHero } from '@/widgets/parallax-hero/parallax-hero'
 
 export default function Statistic() {
-	return (
-		<div>
-			<Header />
-			<ParallaxHero
-				title='Historical Data'
-				subtitle='By studying historical data, organizations can understand patterns and learn from past experiences to improve future outcomes.'
-			/>
-			<Historical />
-			<Footer />
-		</div>
-	)
+  return (
+    <div>
+      <Header />
+      <ParallaxHero
+        title="Historical Data"
+        subtitle="By studying historical data, organizations can understand patterns and learn from past experiences to improve future outcomes."
+      />
+      <Historical />
+      <Footer />
+    </div>
+  )
 }
